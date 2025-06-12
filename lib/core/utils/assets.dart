5918561ce7dca8e039f6a8dd02620febf20f4d3f
Osama Saeid <osamasaeed283@gmail.com>
2025-06-12 17:42:46 +0300

    finish splash UI

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAppIcon
+  /// assets/images/app_icon.png
+  static const String imagesAppIcon = "assets/images/app_icon.png";
+
+  /// Assets for imagesLogo
+  /// assets/images/logo.svg
+  static const String imagesLogo = "assets/images/logo.svg";
+
+  /// Assets for imagesSplashBottom
+  /// assets/images/splash_bottom.svg
+  static const String imagesSplashBottom = "assets/images/splash_bottom.svg";
+
+  /// Assets for imagesSplashTop
+  /// assets/images/splash_top.svg
+  static const String imagesSplashTop = "assets/images/splash_top.svg";
+}
+
