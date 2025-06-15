cb95cc248f8dcbae16541319eaa044209c69c821
Osama Saeid <osamasaeed283@gmail.com>
2025-06-15 05:30:10 +0300

    add firebase to the project

--- lib/core/widgets/custom_app_bar.dart
+++ lib/core/widgets/custom_app_bar.dart
@@ -3,6 +3,7 @@ import 'package:fruits_hub/core/utils/app_text_styles.dart';
 
 AppBar buildAppBar(context, title) {
   return AppBar(
+    elevation: 0,
     backgroundColor: Colors.white,
     centerTitle: true,
     title: Text(title, style: AppTextStyles.bold19),
