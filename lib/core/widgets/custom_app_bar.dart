d65675cf857a13c9f5431ce11c9ad75c35946838
Osama Saeid <osamasaeed283@gmail.com>
2026-09-10 17:49:19 +0200

    feat: initialize Android build configuration, update project dependencies, and add core utility services and widgets.

--- lib/core/widgets/custom_app_bar.dart
+++ lib/core/widgets/custom_app_bar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
 
-AppBar buildAppBar(context, title) {
+AppBar buildAppBar(BuildContext context, String title) {
   return AppBar(
     elevation: 0,
     backgroundColor: Colors.white,
